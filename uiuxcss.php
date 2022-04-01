abd69529898264016b874e77cad2ed122974a2ef
Ulinnuha <ulinnuha@dr.com>
2022-04-01 09:05:42 +0700

    edit login box, make it center

--- uiuxcss.php
+++ uiuxcss.php
@@ -6,29 +6,31 @@
 </head>
 <body>
     
-    <div class="logintitle">
-        Login
-    </div>
+    <div class="loginbox">
+        <div class="logintitle">
+            Login
+        </div>
 
-    <div>
-        <table>
-            <tr>
-                <td>User ID:</td>
-                <td><input type="text" id="xuid" name="xuid"></td>
-            </tr>
-            <tr>
-                <td>Password:</td>
-                <td><input type="password" id="xpsw" name="xpsw"></td>
-            </tr>
-            <tr>
-                <td>&nbsp;</td>
-                <td><input class="loginbtn" type="submit" value="login" name="xuid"></td>
-            </tr>
-            <tr>
-                <td>&nbsp;</td>
-                <td><a href="#">Forget Password</a></td>
-            </tr>
-        </table>
+        <div>
+            <table>
+                <tr>
+                    <td>User ID:</td>
+                    <td><input type="text" id="xuid" name="xuid"></td>
+                </tr>
+                <tr>
+                    <td>Password:</td>
+                    <td><input type="password" id="xpsw" name="xpsw"></td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td><input class="loginbtn" type="submit" value="login" name="xuid"></td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td><a href="#">Forget Password</a></td>
+                </tr>
+            </table>
+        </div>
     </div>
 
 </body>
