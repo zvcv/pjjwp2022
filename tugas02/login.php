717230efefa23a66b6805b41b6b1e2393154d6d8
Ulinnuha <ulinnuha@dr.com>
2022-04-02 14:02:05 +0700

    login page done

---
+++ tugas02/login.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>Husni A Ulinnuha | Tugas UI UX</title>
+
+    <link rel="icon" href="https://cdn-icons.flaticon.com/png/512/4284/premium/4284504.png?token=exp=1648801215~hmac=c367dd0aea6cc071876e39d8357cdfbb">
+    <!-- <a href="https://www.flaticon.com/free-icons/ecology-and-environment" title="ecology-and-environment icons">Ecology-and-environment icons created by Freepik - Flaticon</a> -->
+
+    <link rel="stylesheet" href="lib/login.css">
+
+</head>
+<body>
+    
+    <div class="loginbox">
+
+        <div class="loginboxleft">
+
+            <table>
+
+                <tr>
+                    <td>
+                        <img src="https://cdn-icons-png.flaticon.com/512/4359/4359906.png" alt="computer sticker">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td class="lorem">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt itaque ipsum cupiditate corporis expedita culpa, ratione placeat repellendus totam sapiente tempora fugit, tenetur earum eligendi suscipit nobis architecto nam eius?</td>
+                </tr>
+
+                <tr>
+                    <td>&nbsp;</td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <a href="https://www.flaticon.com/free-sticker/digital-painting_4359906" title="computer stickers" target="_blank">Computer stickers created by Stickers - Flaticon</a>
+                    </td>
+                </tr>
+                
+                <tr>
+                    <td>&nbsp;</td>
+                </tr>
+
+            </table>
+
+        </div>
+
+        <div class="loginboxright">
+
+            <div class="signinbox">
+                
+                <div class="signin-msg">
+                    Welcome to Tugas UI UX
+                </div>
+                
+                <div class="logbox">
+
+                    <div class="textbox">
+                        <input type="text" placeholder="Username">
+                    </div>
+
+                    <div class="textbox">
+                        <input type="password" placeholder="Password">
+                    </div>
+
+                </div>
+
+                <div class="forgot">
+                    <a href="#">Forgot Password?</a>
+                </div>
+
+                <a href="home.php"><input class="logbtn" type="button" value="Sign in"></a>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="footer">
+        Husni A Ulinnuha | 2022
+    </div>
+
+</body>
+</html>
